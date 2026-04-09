35479d278ca9fa5730ec176aab55449581800714
kanishkupadhyay1 <kanishk.upadhyay2006@gmail.com>
2026-04-09 09:04:08 +0530

    UC19

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -2,25 +2,46 @@ import java.util.*;
 
 
 public class TrainConsistManagementApp {
-    public static boolean linearSearch(String[] arr, String key) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].equals(key)) {
-                return true; // match found
+    public static boolean binarySearch(String[] arr, String key) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            int result = key.compareTo(arr[mid]);
+
+            if (result == 0) {
+                return true; // found
+            } else if (result > 0) {
+                low = mid + 1; // search right
+            } else {
+                high = mid - 1; // search left
             }
         }
+
         return false; // not found
     }
-
 
     public static void main(String[] args) {
         String[] bogieIds = {"BG101", "BG205", "BG309", "BG412", "BG550"};
 
+        Arrays.sort(bogieIds);
+
+        System.out.println("Before Sorting:");
+        System.out.println(Arrays.toString(bogieIds));
+
+// Sort first (handles unsorted input case)
+        Arrays.sort(bogieIds);
         Scanner sc = new Scanner(System.in);
+        System.out.println("After Sorting:");
+        System.out.println(Arrays.toString(bogieIds));
+
         System.out.print("Enter Bogie ID to search: ");
-        String searchKey = sc.nextLine();
+        String key = sc.nextLine();
 
+        boolean found = binarySearch(bogieIds, key);
 
-        boolean found = linearSearch(bogieIds, searchKey);
 
         if (found) {
             System.out.println("Bogie ID found in the train.");
