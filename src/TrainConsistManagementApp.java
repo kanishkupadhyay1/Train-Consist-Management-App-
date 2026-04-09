368168da77ea699744d1ce45528eed9ea0f4fbb1
kanishkupadhyay1 <kanishk.upadhyay2006@gmail.com>
2026-04-09 09:00:44 +0530

    UC18

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -2,18 +2,33 @@ import java.util.*;
 
 
 public class TrainConsistManagementApp {
+    public static boolean linearSearch(String[] arr, String key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(key)) {
+                return true; // match found
+            }
+        }
+        return false; // not found
+    }
+
 
     public static void main(String[] args) {
-        String[] bogieNames = {"Sleeper", "AC Chair", "First Class", "General", "Luxury"};
+        String[] bogieIds = {"BG101", "BG205", "BG309", "BG412", "BG550"};
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Bogie ID to search: ");
+        String searchKey = sc.nextLine();
 
 
-        System.out.println("Before Sorting:");
-        System.out.println(Arrays.toString(bogieNames));
+        boolean found = linearSearch(bogieIds, searchKey);
 
-        Arrays.sort(bogieNames);
+        if (found) {
+            System.out.println("Bogie ID found in the train.");
+        } else {
+            System.out.println("Bogie ID not found.");
+        }
 
-        System.out.println("After Sorting:");
-        System.out.println(Arrays.toString(bogieNames));
+
 
 
 
