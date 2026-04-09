bb339f958168ebccc033b1e326e2110302107522
kanishkupadhyay1 <kanishk.upadhyay2006@gmail.com>
2026-04-09 09:08:27 +0530

    UC20

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -26,27 +26,29 @@ public class TrainConsistManagementApp {
     public static void main(String[] args) {
         String[] bogieIds = {"BG101", "BG205", "BG309", "BG412", "BG550"};
 
-        Arrays.sort(bogieIds);
 
-        System.out.println("Before Sorting:");
-        System.out.println(Arrays.toString(bogieIds));
 
-// Sort first (handles unsorted input case)
-        Arrays.sort(bogieIds);
         Scanner sc = new Scanner(System.in);
-        System.out.println("After Sorting:");
-        System.out.println(Arrays.toString(bogieIds));
 
         System.out.print("Enter Bogie ID to search: ");
         String key = sc.nextLine();
+        try {
+
+            if (bogieIds.length == 0) {
+                throw new IllegalStateException("No bogies available in the train. Cannot perform search.");
+            }
 
         boolean found = binarySearch(bogieIds, key);
 
 
-        if (found) {
-            System.out.println("Bogie ID found in the train.");
-        } else {
-            System.out.println("Bogie ID not found.");
+            if (found) {
+                System.out.println("Bogie ID found in the train.");
+            } else {
+                System.out.println("Bogie ID not found.");
+            }
+
+        } catch (IllegalStateException e) {
+            System.out.println("Error: " + e.getMessage());
         }
 
 
