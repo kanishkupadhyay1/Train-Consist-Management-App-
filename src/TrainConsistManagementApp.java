a85c952f9e694bd41d5243cf5de7ee725d3cd54a
kanishkupadhyay1 <kanishk.upadhyay2006@gmail.com>
2026-04-09 08:57:44 +0530

    UC17

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -3,40 +3,22 @@ import java.util.*;
 
 public class TrainConsistManagementApp {
 
-    public static void bubbleSort(int[] arr) {
-        int n = arr.length;
-
-        for (int i = 0; i < n - 1; i++) {
-            boolean swapped = false;
-
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    // swap
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-
-                    swapped = true;
-                }
-            }
-
-            // optimization: stop if already sorted
-            if (!swapped) break;
-        }
-    }
     public static void main(String[] args) {
-        int[] capacities = {72, 56, 24, 70, 60};
-
-
+        String[] bogieNames = {"Sleeper", "AC Chair", "First Class", "General", "Luxury"};
 
 
         System.out.println("Before Sorting:");
-        System.out.println(Arrays.toString(capacities));
+        System.out.println(Arrays.toString(bogieNames));
 
-        bubbleSort(capacities);
+        Arrays.sort(bogieNames);
 
         System.out.println("After Sorting:");
-        System.out.println(Arrays.toString(capacities));
+        System.out.println(Arrays.toString(bogieNames));
+
+
+
+
+
     }
 }
 
